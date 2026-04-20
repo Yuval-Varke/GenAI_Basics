34b48216aa3e2ac753ce836f413e08b7d2b5f269
Yuval Varke <varke.vishu01@gmail.com>
2026-04-20 19:38:56 +0530

    Implemented Short term memory

--- main.py
+++ main.py
@@ -6,10 +6,26 @@ load_dotenv()
 
 client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
-res = client.models.generate_content(
-    model="gemini-2.5-flash",
-    contents="What is the capital of India?"
-)
+messages = []
 
-print(res.text)
+while True:
+    user_input = input("Enter your question: ")
+    if user_input.lower() in ["exit", "quit"]:
+        break
+
+    messages.append({
+        "role":"user",
+        "content":user_input
+    })
+    
+    res = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=list(map(lambda message:message["role"] + " : " + message["content"], messages)),
+    )
+
+    messages.append({
+        "role":"AI Assistant",
+        "content":res.text
+    })
+    print(res.text)
 
