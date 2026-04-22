7d7c86e2fadc8adf45bf073d26bd72fb74574916
Yuval Varke <varke.vishu01@gmail.com>
2026-04-22 17:45:32 +0530

    Gemini compatibility with OpenAI code

---
+++ gemini_openai.py
@@ -0,0 +1,21 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+client = OpenAI(
+    api_key=os.getenv("GEMINI_API_KEY"),
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
+
+response = client.chat.completions.create(
+    model="gemini-2.5-flash",
+    messages=[
+        {   "role": "user",
+            "content": "Hey There, I am Yuval Varke!"
+        }
+    ]
+)
+
+print(response.choices[0].message.content)
